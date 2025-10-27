19f9ee4712c19dcaedc845e9d3eedb5e5387e1e5
madhav <madhavofficial.259@gmail.com>
2025-10-27 15:53:25 +0530

    Admin commit

--- src/services/productApi.js
+++ src/services/productApi.js
@@ -1,3 +1,30 @@
+// import api from "./api";
+
+// // ✅ Fetch all products (public)
+// export const fetchAllProducts = async () => {
+//   const res = await api.get("/products/find-all");
+//   return res.data;
+// };
+
+// // ✅ Add product (admin only)
+// export const addProduct = async (product) => {
+//   const res = await api.post("/products/add", product);
+//   return res.data;
+// };
+
+// // ✅ Update product (admin only)
+// export const updateProduct = async (product) => {
+//   const res = await api.put("/products/update", product);
+//   return res.data;
+// };
+
+// // ✅ Delete product (admin only)
+// export const deleteProduct = async (product) => {
+//   const res = await api.delete("/products/delete", { data: product });
+//   return res.data;
+// };
+
+
 import api from "./api";
 
 // ✅ Fetch all products (public)
