992e5bea9fa716876a5178283244ba7b5b313034
madhav <madhavofficial.259@gmail.com>
2025-11-10 21:13:36 +0530

    looking

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -134,7 +134,7 @@ function ACList() {
         </button>
       </div>
 
-      <div className="grid md:grid-cols-3 sm:grid-cols-2 gap-4">
+      <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {filteredProducts.slice(0, visibleCount).map((p) => (
           <div
             key={p.id}
